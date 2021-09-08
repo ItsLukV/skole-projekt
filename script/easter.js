d8f0b5cff6eaeb2f253de7afd67c53ca817e0b94
dendersen <74113558+dendersen@users.noreply.github.com>
2021-09-08 10:55:54 +0200

    Update easter.js
    made easter-egg a lot more responsive

--- script/easter.js
+++ script/easter.js
@@ -1,40 +1,40 @@
 var keyS = 0
 function easterTime(){
     if(keyIsPressed === true){
-        if(keyS==0){
+        switch(keyS){
+        case 0:
+        case 3:
             if(keyCode === 69){
-                keyS = 1
+                keyS +=1
                 keyCode = 0
             }else if(keyCode==0){}else{
                 keyS = 0
             }
-       }else if(keyS==1){
-           if(keyCode=== 83){
-               keyS = 2
+        break;
+        case 1:
+            if(keyCode=== 83){
+               keyS +=1
                keyCode = 0
            }else if(keyCode==0){}else{
-            keyS = 0
-        }
-       }else if(keyS==2){
+            keyS = 0}
+        break;
+       case 2:
            if(keyCode===84){
-               keyS=3
+               keyS+=1
                keyCode=0
            }else if(keyCode==0){}else{
             keyS = 0
         }
-       } else if(keyS==3){
-           if(keyCode===69){
-               keyS = 4
-               keycode=0
-           }else if(keyCode==0){}else{
-            keyS = 0
-        }
-       }else if(keyS==4){
+        break;
+       case 4:
            if(keyCode===82){
                keyS=5
                keyCode=0
+           }else if(keyCode==0){}else{
+            keyS = 0
            }
-       }else if(keyS==5){
+           break;
+        case 5:
            if(keyCode===13){
                keyS=0
                keyCode=0
@@ -43,6 +43,6 @@ function easterTime(){
            }else if(keyCode==0){}else{
            keyS = 0
            }
-       }
+        }
     }
 }
